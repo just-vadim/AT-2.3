92a23d20772add7c11bd4680ca376d184de39398
Vadim <vadim.kurn@gmail.com>
2020-11-11 21:53:53 +0300

    Test fail

--- src/test/java/ru/netology/test/PostmanEchoPostTest.java
+++ src/test/java/ru/netology/test/PostmanEchoPostTest.java
@@ -1,4 +1,21 @@
 package ru.netology.test;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostmanEchoPostTest {
+    @Test
+    public void shouldPost(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("test body")
+        .when()
+                .post("/post")
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("body"))
+        ;
+    }
 }
